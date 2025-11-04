58c463ade3e0318f2b0a6142ff0180d75e34f6dd
GreyCatProductions <GreyCatProductionsOriginal@gmail.com>
2025-11-04 08:57:54 +0100

    forgot to remove early return

--- Few_Shot_Training.py
+++ Few_Shot_Training.py
@@ -19,7 +19,7 @@ def prepare_data(training_data_filtered, combined_into_column, label_column):
                                                combined_into_column, label_column, translation)
 
 def main():
-    data_already_prepared = False
+    data_already_prepared = True
     base_model_location = Path("downloaded_model")
     model_save_location = Path("classification_data_align_V1")
     training_data_filtered = Path("Training_Data_Filtered") #dataset path
@@ -29,7 +29,6 @@ def main():
     if not data_already_prepared:
         prepare_data(training_data_filtered, combined_column, label_column)
 
-    return
     cross_validate = True
     if cross_validate:
         print("Starting optuna cross validation training...")
